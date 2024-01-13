976560031f757f63141ac14ae5e9bf7eeb12adf2
5Polosok <140302167+5Polosok@users.noreply.github.com>
2024-01-13 13:15:55 +0500

    Update nbb.lua

--- nbb.lua
+++ nbb.lua
@@ -199,7 +199,7 @@ misc:Toggle{
             for _,v in pairs(workspace.ItemSpawns.StandardItems:GetChildren()) do
                 if v:GetChildren()[1] ~= nil then
                     pcall(function()
-                        if  (v:GetChildren()[1]:GetPivot().p - game.Players.LocalPlayer.Character:GetPivot().p).Magnitude > 5 then
+                        if  math.abs(v:GetChildren()[1]:GetPivot().p - game.Players.LocalPlayer.Character:GetPivot().p).Magnitude > 10 then
                             plyr.Character.HumanoidRootPart.CFrame = v:GetChildren()[1].CFrame * CFrame.new(0, 0, -4)
                         end
                         fireproximityprompt(v:GetChildren()[1].ProximityAttachment.Interaction)
