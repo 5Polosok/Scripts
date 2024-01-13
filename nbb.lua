73c83d5eb56ba2f6818fd8fda753a2cc05409ccb
5Polosok <140302167+5Polosok@users.noreply.github.com>
2024-01-13 14:01:29 +0500

    Update nbb.lua

--- nbb.lua
+++ nbb.lua
@@ -198,24 +198,19 @@ misc:Toggle{
         while ItemFarm do 
             for _,v in pairs(workspace.ItemSpawns.StandardItems:GetChildren()) do
                 if v:GetChildren()[1] ~= nil then
-                    if not ItemFarm then break end
-                    plyr.Character.HumanoidRootPart.CFrame = v:GetChildren()[1].CFrame * CFrame.new(0, 0.5, 0)
-                    repeat
-                        if not ItemFarm then break end
-                        task.wait()
-                        if v:GetChildren()[1].ProximityAttachment.Interaction then 
-                            print("yes")
-                        else 
-                            print("nuh uh")
-                        end
-                    until v:GetChildren()[1].ProximityAttachment.Interaction
-                    task.wait(0.5)
-                    fireproximityprompt(v:GetChildren()[1].ProximityAttachment.Interaction)
-                    repeat
-                        if not ItemFarm then break end
-                        task.wait()
-                    until not v:GetChildren()[1].ProximityAttachment.Interaction
-                    -- task.wait(3)
+                    pcall(function()
+                        plyr.Character.HumanoidRootPart.CFrame = v:GetChildren()[1].CFrame * CFrame.new(0, 0.5, 0)
+                        repeat
+                            if not ItemFarm then break end
+                            task.wait()
+                        until v:GetChildren()[1].ProximityAttachment.Interaction
+                        task.wait(0.5)
+                        fireproximityprompt(v:GetChildren()[1].ProximityAttachment.Interaction)
+                        repeat
+                            if not ItemFarm then break end
+                            task.wait()
+                        until not v:GetChildren()[1]
+                    end)
                 end
             end 
             task.wait()      
