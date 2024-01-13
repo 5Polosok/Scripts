7283de54fcbc44b19f7f6e3d68c18789bf8f3bf4
5Polosok <140302167+5Polosok@users.noreply.github.com>
2024-01-13 17:04:35 +0500

    Update nbb.lua

--- nbb.lua
+++ nbb.lua
@@ -367,7 +367,7 @@ Main:Toggle{
         plyr.Character.HumanoidRootPart.CFrame = CFrame.new(2006,942,-1443)
         while AFarm do
             for _,v in pairs(workspace.Living:GetChildren()) do
-                if (v:GetPivot().p-game.Players.LocalPlayer.Character:GetPivot().p).Magnitude <= 400 then
+                if tostring(v) ~= tostring(game.Players.LocalPLayer) and (v:GetPivot().p-game.Players.LocalPlayer.Character:GetPivot().p).Magnitude <= 400 then
                     repeat
                         if not AFarm then break end
                         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.PrimaryPart.CFrame + v.PrimaryPart.CFrame.lookVector * 5
