aa5dbe96b2d2ba6ebefaf958485121b82ab9d016
5Polosok <140302167+5Polosok@users.noreply.github.com>
2024-01-17 10:33:34 +0500

    Update APS.lua

--- APS.lua
+++ APS.lua
@@ -480,7 +480,7 @@ Main2:Toggle{
                     
                     game:GetService("ReplicatedStorage").Bridge:FireServer(unpack(args))
                     task.wait()
-                until mob
+                until mob:GetAttributes("Health") == 0
             end
             task.wait()
         end
@@ -556,7 +556,7 @@ Main2:Toggle{
                     }
                     game:GetService("ReplicatedStorage").Bridge:FireServer(unpack(args))
                     task.wait()
-		until mob
+		until mob:GetAttributes()["Health"] == 0
             end
             task.wait()
         end
@@ -635,7 +635,7 @@ Main2:Toggle{
                     
                     game:GetService("ReplicatedStorage").Bridge:FireServer(unpack(args))
                     task.wait()
-                until mob
+                until mob:GetAttributes()["Health"] == 0
             end
             task.wait()
         end
