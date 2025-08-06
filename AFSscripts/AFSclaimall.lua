df136671076ef89a2c4b44972d4d078b9cd79566
5Polosok <140302167+5Polosok@users.noreply.github.com>
2025-08-06 13:42:12 +0500

    Update AFSclaimall.lua

--- AFSscripts/AFSclaimall.lua
+++ AFSscripts/AFSclaimall.lua
@@ -1,35 +1,70 @@
-repeat task.wait() until game:IsLoaded()
-local codes = {"FORGOT40KLIKES", "THANKSFOR50KLIKES", "UPDATEONE", "BONIMARU", "MBFORDELAY", "15MVISITS", "100KBLUEAPP", "SORRYFORTHESHUTDOWN", "THANKSFORALLTHESUPPORT", "UPDATEHALF", "TOBITO",  "RELEASE", "THANKSFOR20KLIKES", "THANKSFOR5KLIKES", "SORRY4SHUTDOWN", "15KCCU", "TRAILER", "THANKSFOR20K", "ILUVAFS"}
 --Redeem codes from table
-if getgenv().RedeemCodes then
+local RedeemCodesFunction = function()
 	for _,v in pairs(codes) do
 		local args = {
 			"RedeemCode",
 			v
 		}
-		game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RemoteFunction"):InvokeServer(unpack(args))
+		local suc, fail = pcall(function()
+			game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RemoteFunction"):InvokeServer(unpack(args))
+		end)
+		if suc then print("Successfully redeemed code: "..v) else print("Error while redeeming code: "..fail)
 	end
 end
 --Collect Daily reward
-if getgenv().CollectDaily then
-	game:GetService("ReplicatedStorage"):WaitForChild("LobbyRemotes"):WaitForChild("DailyRewards"):FireServer()
+local CollectDailyFunction = function()
+	local suc, failure = pcall(function()
+		game:GetService("ReplicatedStorage"):WaitForChild("LobbyRemotes"):WaitForChild("DailyRewards"):FireServer()
+	end)
+	if suc then print("Success to collect daily") else print("Error to collect daily reward: "..failure) end
 end
 --Spin fortune wheel
-if getgenv().BuyRRS then
-	local args = {
-		true
-	}
-	game:GetService("ReplicatedStorage"):WaitForChild("LobbyRemotes"):WaitForChild("SpinWheelFunction"):InvokeServer(unpack(args))
-end
---Buy goldshop
-if getgenv().SpinWheel then
-	for i=1,5 do
+local SpinWheelFunction = function()
+	if getgenv().SpinWheel then
 		local args = {
-			"BuyShopItem",
-			"GoldShop",
-			7,
-			1
+			true
 		}
-		game:GetService("ReplicatedStorage"):WaitForChild("LobbyRemotes"):WaitForChild("ShopRemote"):FireServer(unpack(args))
+		local suc,failure = pcall(function()
+			game:GetService("ReplicatedStorage"):WaitForChild("LobbyRemotes"):WaitForChild("SpinWheelFunction"):InvokeServer(unpack(args))
+		end)
+		if suc then print("Spinned wheel") else print("Error to spin wheel: "..failure) end
 	end
 end
+--Buy goldshop rr
+local BuyRRFunction = function()
+	if getgenv().BuyRRS then
+		for i=1,5 do
+			local args = {
+				"BuyShopItem",
+				"GoldShop",
+				7,
+				1
+			}
+			local suc, er = pcall(function()
+				game:GetService("ReplicatedStorage"):WaitForChild("LobbyRemotes"):WaitForChild("ShopRemote"):FireServer(unpack(args))
+			end)
+			if suc then print("Bought RR from shop") else print("Error to buy RR: "..er) end
+		end
+	end
+end
+local main = function()
+    if getgenv().BuyRRS then BuyRRFunction() end
+	if getgenv().CollectDaily then CollectDailyFunction() end
+	if getgenv().SpinWheel then SpinWheelFunction() end
+	if getgenv().RedeemCodes then RedeemCodesFunction() end
+end
+
+task.spawn(function()
+    if not game:IsLoaded() then
+        game.Loaded:Wait()
+    end
+    task.wait(3)
+    
+    if Players.LocalPlayer then
+		main()
+    else
+        Players:GetPropertyChangedSignal("LocalPlayer"):Wait()
+        task.wait(1)
+        main()
+    end
+end)
